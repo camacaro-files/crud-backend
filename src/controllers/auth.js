58b17dd554582938dd0be8d9bb94be1dba5f1f1e
camacaro-files <camacaro.a.p.r@gmail.com>
2026-04-06 17:00:55 -0400

    fix: cambiar adaptador de sqlite a postgresql

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSql } from '@prisma/adapter-libsql'
+import { PrismaPg } from '@prisma/adapter-pg'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
-const adapter = new PrismaLibSql({ url: 'file:./prisma/dev.db' })
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const prisma = new PrismaClient({ adapter })
 
 export const register = async (req, res) => {
