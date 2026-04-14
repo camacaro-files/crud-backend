0057e3e31dc730b835340eb146e4cf3bf136a81e
camacaro-files <camacaro.a.p.r@gmail.com>
2026-04-13 22:10:13 -0400

    actualizar cors con url de vercel

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-import 'dotenv/config'  // ← debe ser la primera línea
+import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 import productRoutes from './routes/products.js'
 import authRoutes from './routes/auth.js'
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
-// Middleware: le dice a Express que entienda JSON en las peticiones
+app.use(cors({
+  origin: [
+    'http://localhost:5174',
+    'https://crud-frontend-pi-five.vercel.app/' // ← tu URL real de Vercel
+  ],
+  credentials: true
+}))
+
 app.use(express.json())
-
-// Registra las rutas de productos bajo el prefijo /api/products
 app.use('/api/auth', authRoutes)
 app.use('/api/products', productRoutes)
 
-// Ruta raíz para verificar que el servidor funciona
 app.get('/', (req, res) => {
   res.json({ message: '¡Servidor funcionando!' })
 })
